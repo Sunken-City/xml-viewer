e4f2a1bd4455fc4d56bbefaf34cf7aaa336b5054
unknown <acloudy@mail.smu.edu>
2013-08-01 14:35:09 -0500

    First project I worked on for the Internship. This also happens to be my first GUI. Not only can you read XML files in a nice treeview, but you can also make different color themes for the viewer.

---
+++ XML Viewer/XML Viewer/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace XML_Viewer
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ExpandMenu(Ellipse item, int distance)
+		{
+			DoubleAnimation animation = new DoubleAnimation(0, distance, TimeSpan.FromSeconds(.5));
+			TranslateTransform move = new TranslateTransform();
+			item.RenderTransform = move;
+			move.BeginAnimation(TranslateTransform.XProperty, animation);
+			move.BeginAnimation(TranslateTransform.YProperty, animation);
+		}
+
+		private void XML_Viewer_Loaded(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ellipse1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+
+		}
+
+		private void ellipse1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+
+		}
+	}
+}
